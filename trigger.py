cc076e8511cf8c6860ce629993d0d2645fa3ca74
Kirill Melnikov <kir@melniki.pro>
2016-10-20 20:07:01 +0500

    env vars update

--- trigger.py
+++ trigger.py
@@ -2,11 +2,15 @@
 import requests
 import os
 
-token = os.getenv("TOKEN")
+token = os.getenv("DOCKER_HUB_TOKEN")
 version = os.getenv("VERSION")
 
-if token==None or version==None:
-    print "env not set"
+if token==None:
+    print "env DOCKER_HUB_TOKEN not set"
+    exit(1)
+
+if version==None:
+    print "env VERSION not set"
     exit(1)
 
 url     = "https://registry.hub.docker.com/u/skbkontur/kibana/trigger/%s/" % token
